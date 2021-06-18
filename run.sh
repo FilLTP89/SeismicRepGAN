14769b2e8be2f87e7d1aa0bb2089607bee19480f
kltpk89 <filippo.gatti@centralesupelec.fr>
2021-06-18 09:26:39 +0200

    Run with new config
    Signed-off-by: kltpk89 <filippo.gatti@centralesupelec.fr>

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-python3 RepGAN.py --dataroot /home/kltpk89/Data/Filippo/aeolus/mdof/damaged_1_5P --cuda
-
+python3 RepGAN.py --dataroot /home/kltpk89/Data/Filippo/aeolus/mdof/damaged_1_8P --nX 512 --cuda --epochs 400 --kernel 3 --stride 2 --nAElayers 3 --nZfirst 8 --latentSdim 256 --latentNdim 64 --nSlayers 1 --nClayers 1 --nNlayers 1 --Sstride 2 --Cstride 2 --Nstride 2 --Sstride 2 --Skernel 3 --Ckernel 3 --Nkernel 3
