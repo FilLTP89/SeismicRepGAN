ca2351dc8cf39b91d40b37d4be28ba0877d86d6b
kltpk89 <filippo.gatti@centralesupelec.fr>
2021-06-18 09:22:42 +0200

    [UPDATE] parser
    Signed-off-by: kltpk89 <filippo.gatti@centralesupelec.fr>

--- RepGAN.py
+++ RepGAN.py
@@ -96,15 +96,28 @@ def make_or_restore_model():
 def ParseOptions():
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs",type=int,default=100000,help='Number of epochs')
-    parser.add_argument("--kernel",type=int,default=3,help='CNN kernel size')
-    parser.add_argument("--stride",type=int,default=2,help='CNN stride')
-    parser.add_argument("--nAElayers",type=int,default=5,help='Number of AE CNN layers')
-    parser.add_argument("--nDlayers",type=int,default=10,help='Number of D CNN layers')
     parser.add_argument("--Xsize",type=int,default=1024,help='Data space size')
     parser.add_argument("--nX",type=int,default=512,help='Number of signals')
     parser.add_argument("--nXchannels",type=int,default=2,help="Number of data channels")
-    parser.add_argument("--latentZdim",type=int,default=1024,help="Latent space dimension")
-    parser.add_argument("--batchSize",type=int,default=128,help='input batch size')
+    parser.add_argument("--nAElayers",type=int,default=3,help='Number of AE CNN layers')
+    parser.add_argument("--nDlayers",type=int,default=10,help='Number of D CNN layers')
+    parser.add_argument("--kernel",type=int,default=3,help='CNN kernel size')
+    parser.add_argument("--stride",type=int,default=2,help='CNN stride')
+    parser.add_argument("--nZfirst",type=int,default=8,help="Initial number of channels")
+    parser.add_argument("--branching",type=str,default='conv',help='conv or dens')
+    parser.add_argument("--latentSdim",type=int,default=256,help="Latent space s dimension")
+    parser.add_argument("--latentCdim",type=int,default=5,help="Number of classes")
+    parser.add_argument("--latentNdim",type=int,default=128,help="Latent space n dimension")
+    parser.add_argument("--nSlayers",type=int,default=3,help='Number of S-branch CNN layers')
+    parser.add_argument("--nClayers",type=int,default=3,help='Number of C-branch CNN layers')
+    parser.add_argument("--nNlayers",type=int,default=3,help='Number of N-branch CNN layers')
+    parser.add_argument("--Skernel",type=int,default=7,help='CNN kernel of S-branch branch')
+    parser.add_argument("--Ckernel",type=int,default=7,help='CNN kernel of C-branch branch')
+    parser.add_argument("--Nkernel",type=int,default=7,help='CNN kernel of N-branch branch')
+    parser.add_argument("--Sstride",type=int,default=4,help='CNN stride of S-branch branch')
+    parser.add_argument("--Cstride",type=int,default=4,help='CNN stride of C-branch branch')
+    parser.add_argument("--Nstride",type=int,default=4,help='CNN stride of N-branch branch')
+    parser.add_argument("--batchSize",type=int,default=128,help='input batch size')    
     parser.add_argument("--nCritic",type=int,default=5,help='number of discriminator training steps')
     parser.add_argument("--clipValue",type=float,default=0.01,help='clip weight for WGAN')
     parser.add_argument("--dataroot",type=str,default="/gpfs/workdir/invsem07/damaged_1_8P",help="Data root folder")
@@ -117,18 +130,23 @@ def ParseOptions():
     parser.add_argument("--cuda",action='store_true',default=False,help='Use cuda powered GPU')
     options = parser.parse_args().__dict__
 
-    options['Xshape'] = (options['Xsize'], options['nXchannels'])
     options['batchXshape'] = (options['batchSize'],options['Xsize'],options['nXchannels'])
-    options['latentCidx'] = list(range(512))
-    options['latentSidx'] = list(range(512,517))
-    options['latentNidx'] = list(range(517,options['latentZdim']))
-    options['latentCdim'] = len(options['latentCidx'])
-    options['latentSdim'] = len(options['latentSidx'])
-    options['latentNdim'] = len(options['latentNidx'])
+    options['Xshape'] = (options['Xsize'],options['nXchannels'])
+
+    options['latentZdim'] = options['latentSdim']+options['latentNdim']+options['latentCdim']
+
     options['Zsize'] = options['Xsize']//(options['stride']**options['nAElayers'])
-    options['nZchannels'] = options['latentZdim']//options['Zsize']
-    options['nSchannels'] = options['latentSdim']//options['Zsize']
-    options['Sshape'] = (options['Zsize'], options['nSchannels'])
+    options['nZchannels'] = options['nZfirst']*(options['stride']**options['nAElayers']) # options['latentZdim']//options['Zsize']
+    options['nZshape'] = (options['Zsize'],options['nZchannels'])
+
+    options['nSchannels'] = options['nZchannels']*options['Sstride']**options['nSlayers']
+    options['nCchannels'] = options['nZchannels']*options['Cstride']**options['nClayers']
+    options['nNchannels'] = options['nZchannels']*options['Nstride']**options['nNlayers']
+    options['Ssize'] = int(options['Zsize']*options['Sstride']**(-options['nSlayers']))
+    options['Csize'] = int(options['Zsize']*options['Cstride']**(-options['nClayers']))
+    options['Nsize'] = int(options['Zsize']*options['Nstride']**(-options['nNlayers']))
+
+    options['nDlayers'] = min(options['nDlayers'],int(mt.log(options['Xsize'],options['stride'])))
 
     return options
 
@@ -231,9 +249,6 @@ class RepGAN(Model):
         """
         self.__dict__.update(options)
 
-        assert self.nZchannels >= 1
-        assert self.nZchannels >= self.stride**self.nAElayers
-        assert self.latentZdim >= self.Xsize//(self.stride**self.nAElayers)
         # define the constraint
         self.ClipD = ClipConstraint(0.01)
         """
