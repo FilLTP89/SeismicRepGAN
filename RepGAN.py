6225c860d74c362fd4670033b5fb5358d2dc393c
invsem07 <filippo.gatti@centralesupelec.fr>
2021-07-13 07:15:52 +0200

    Sampling with logvar
    Signed-off-by: invsem07 <filippo.gatti@centralesupelec.fr>

--- RepGAN.py
+++ RepGAN.py
@@ -192,13 +192,13 @@ class SamplingFxS(Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
 
     def call(self, inputs):
-        # z_mean, z_log_var = inputs
-        z_mean, z_std = inputs
+        #z_mean, z_std = inputs
+        z_mean, z_log_var = inputs
         batch = tf.shape(z_mean)[0]
         dim = tf.shape(z_mean)[1]
         epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-        # return z_mean + tf.exp(0.5 * z_log_var) * epsilon
-        return z_mean + z_std * epsilon
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+        #return z_mean + z_std * epsilon
 
 # clip model weights to a given hypercube
 class ClipConstraint(Constraint):
