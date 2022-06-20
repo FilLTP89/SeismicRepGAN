[BUGFIX] gaussian NLL sign

RepGAN_losses.py
@@ -16,7 +16,7 @@ from tensorflow.keras.optimizers import Adam, RMSprop
 
 ε = 1e-8
 
-def GaussianNLL(x,μ,Σ,mod='var',raxis=None):
+def GaussianNLL(x,μ,σ2,mod='var',raxis=None):
     """
         Gaussian negative loglikelihood loss function
     """
@@ -27,18 +27,20 @@ def GaussianNLL(x,μ,Σ,mod='var',raxis=None):
 
     log2pi = -0.5*n_dims*tf.math.log(2.*np.pi)
 
-    if 'var' in mod:
-      Σ = tf.math.log(Σ+ε)
+    # if 'var' in mod:
+    #   Σ = tf.math.log(Σ+ε)
 
-    mse = -0.5*tf.square(x-μ)*tf.exp(-Σ)
-    traceΣ = -tf.reduce_sum(Σ,axis=raxis)
+    # mse = -0.5*tf.square(x-μ)*tf.exp(-Σ)
+    #traceΣ = -tf.reduce_sum(Σ,axis=raxis)
+    mse = -0.5*tf.square(x-μ)/(tf.math.maximum(σ2,ε))
+    traceΣ = -0.5*tf.reduce_sum(tf.math.log(tf.math.maximum(σ2,ε)),axis=raxis)
     NLL = tf.reduce_sum(mse,axis=raxis)+traceΣ+log2pi
 
     # mse = -0.5*tf.reduce_sum(tf.keras.backend.square((x-μ))/sigma,axis=raxis) 
     # sigma_trace = -0.5*tf.reduce_sum(tf.math.log(sigma), axis=raxis)
     # NLL = mse+sigma_trace+log2pi
 
-    return tf.reduce_mean(NLL)
+    return tf.reduce_mean(-NLL)
 
 def GANLoss(y_true, y_predict):
     # General GAN Loss (for real and fake) with labels: {0,1}. Sigmoid output from D
@@ -104,17 +106,17 @@ def GradientPenalty(batchSize,X,Gz,D):
 def getOptimizers(**kwargs):
     getOptimizers.__globals__.update(kwargs)
     optimizers = {}
-    optimizers['DxOpt'] = Adam(learning_rate=0.002, beta_1=0.5, beta_2=0.9999)
+    optimizers['DxOpt'] = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.9999)
     if 'WGAN' in discriminator:
-        optimizers['DcOpt'] = RMSprop(learning_rate=0.002)
-        optimizers['DsOpt'] = RMSprop(learning_rate=0.002)
-        optimizers['DnOpt'] = RMSprop(learning_rate=0.002)
+        optimizers['DcOpt'] = RMSprop(learning_rate=0.001)
+        optimizers['DsOpt'] = RMSprop(learning_rate=0.001)
+        optimizers['DnOpt'] = RMSprop(learning_rate=0.001)
     else:
-        optimizers['DcOpt'] = Adam(learning_rate=0.002, beta_1=0.5, beta_2=0.9999)
-        optimizers['DsOpt'] = Adam(learning_rate=0.002, beta_1=0.5, beta_2=0.9999)
-        optimizers['DnOpt'] = Adam(learning_rate=0.002, beta_1=0.5, beta_2=0.9999) 
-    optimizers['FxOpt'] = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.9999)
-    optimizers['GzOpt'] = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.9999)
+        optimizers['DcOpt'] = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.9999)
+        optimizers['DsOpt'] = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.9999)
+        optimizers['DnOpt'] = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.9999) 
+    optimizers['FxOpt'] = Adam(learning_rate=0.0005, beta_1=0.5, beta_2=0.9999)
+    optimizers['GzOpt'] = Adam(learning_rate=0.0005, beta_1=0.5, beta_2=0.9999)
     return optimizers
 
 def getLosses(**kwargs):

interferometry.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import windows, detrend, filtfilt, butter, lfilter, resample
 from scipy import interpolate
 from mtcross_2 import MTCross
-import utils as utils
+import interferometry_utils as utils
 
 from random import randint
 