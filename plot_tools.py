1cedea6e5196f66411de20a2cfc8eb86484adc93
Filippo Gatti <filippo.gatti@centralesupelec.fr>
2022-10-14 17:16:47 +0200

    [CLEANUP]
    Signed-off-by: Filippo Gatti <filippo.gatti@centralesupelec.fr>

--- plot_tools.py
+++ plot_tools.py
@@ -16,8 +16,6 @@ cmaps = OrderedDict()
 cmaps['Qualitative'] = ['Pastel1', 'Pastel2', 'Paired', 'Accent','Dark2', 'Set1', 'Set2', 'Set3',
                         'tab10', 'tab20', 'tab20b', 'tab20c']
 from statistics import mean
-import plotly.graph_objects as go
-import plotly.express as px
 from PIL import Image
 import io
 
